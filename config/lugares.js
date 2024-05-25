3b4228fab0ae74761569ca6bcca303b3741942b1
Pablo <pablosantoschavez2001@gmail.com>
2024-05-25 19:33:32 -0400

    Commit1

---
+++ config/lugares.js
@@ -0,0 +1,15 @@
+const lugares = [
+    {nombre:'Fina Estampa', coordenadas: [-36.830509, -73.053467]},
+    {nombre:'Camino del agua', coordenadas: [-36.843524, -73.001441]},
+    {nombre:'Mall del centro Concepción', coordenadas: [-36.825105, -73.045155]},
+    {nombre:'Mall mirador Bio Bio', coordenadas: [-36.830051, -73.063677]},
+    {nombre:'Mall del trebol', coordenadas: [-36.791896, -73.066281]},
+    {nombre:'Rincon Criollo', coordenadas: [-36.839856, -73.006804]},
+    {nombre:'Zoologico Concepción', coordenadas: [-36.84008802855375, -73.00721099741818]},
+    {nombre:'Universidad de Concepción', coordenadas: [-36.8304818711643, -73.03729693876494]},
+    {nombre:'Parque Ecuador', coordenadas: [-36.832944, -73.047742]},
+    {nombre:'Plaza de los dinosaurios', coordenadas: [-36.815163, -73.030797]},
+    {nombre:'Cerro Caracol', coordenadas: [-36.83379987369397, -73.04619492817464]},
+];
+
+
